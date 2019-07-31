198358026bc8dc274062b4d313604c9e49162c48
aaronshivers <aaron@moonmail.org>
2019-07-30 22:43:29 -0500

    added styling

--- src/components/Directory.js
+++ src/components/Directory.js
@@ -3,7 +3,7 @@ import MenuItem from './MenuItem'
 
 const Directory = () => {
 
-  const [ sections, setSections ] = useState([
+  const [ sections ] = useState([
     {
       title: 'hats',
       imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
