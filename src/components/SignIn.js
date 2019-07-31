f94dd3aa2696e46cd0e87454d89ea43674bd6518
aaronshivers <aaron@moonmail.org>
2019-07-31 11:38:57 -0500

    added customButton component

---
+++ src/components/SignIn.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import FormInput from './FormInput'
+import CustomButton from './CustomButton'
+
+const SignInPage = () => {
+  const [ email, setEmail ] = useState('')
+  const [ password, setPassword ] = useState('')
+
+  const handleSignIn = event => {
+    event.preventDefault()
+  }
+
+  return (
+    <div className="sign-in">
+      <h2>I already have an account</h2>
+      <span>Sign in with your email and password</span>
+
+      <form onSubmit={ handleSignIn }>
+        <FormInput
+          name="email"
+          type="email"
+          label="email"
+          autoComplete="email"
+          value={ email }
+          required
+          handleChange={ e => setEmail(e.target.value) }
+        />
+        <FormInput
+          name="password"
+          type="password"
+          label="password"
+          autoComplete="password"
+          value={ password }
+          required
+          handleChange={ e => setPassword(e.target.value) }
+        />
+        <CustomButton>sign in</CustomButton>
+      </form>
+    </div>
+  )
+}
+
+export default SignInPage
