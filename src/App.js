f19fb91839762c3f0d59f753c7671fd1cc3945be
aaronshivers <aaron@moonmail.org>
2019-07-31 08:05:40 -0500

    updated router to use withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import HatsPage from './pages/HatsPage'
+import NotFoundPage from './pages/NotFoundPage'
 
 const App = () => (
-  <>
-    <HomePage />
-  </>
+  <Router>
+    <Switch>
+      <Route path="/" exact component={ HomePage } />
+      <Route path="/shop/hats" component={ HatsPage } />
+      <Route component={ NotFoundPage } />
+    </Switch>
+  </Router>
 )
 
 export default App
