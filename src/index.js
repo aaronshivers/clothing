9ae61c34d30232958f6ae63af189233943c08779
aaronshivers <aaron@moonmail.org>
2019-09-08 20:59:48 -0500

    added userReducer and redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import './styles/styles.scss'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const appProvider = (
+  <Provider>
+    <App />
+  </Provider>
+)
+
+ReactDOM.render(appProvider, document.getElementById('root'))
